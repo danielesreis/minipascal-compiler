e67834e29dd958cd5176bf16beb7a3dfb975f76b
Daniele Reis <danielesreis@outlook.com>
2018-01-19 14:26:31 -0200

    ajeitei alguns tokens

--- src/compilador/Token.java
+++ src/compilador/Token.java
@@ -1,18 +1,21 @@
 package compilador;
 
-public class Tokens {
+public class Token {
     public byte kind;
     public String spelling;
     
-    public Tokens (byte kind, String spelling){
+    public Token (byte kind, String spelling){
             this.kind = kind;
             this.spelling = spelling;
-            if (kind==ID)
-                for (byte k = VAR; k<=PROCEDURE; k++ )
+            
+            if (kind == ID) {
+                for (byte k = PROGRAM; k<=PROCEDURE; k++ ) {
                     if (spelling.equals(spellings[k])){
                         this.kind = k;
                         break;
                     }
+                }
+            }
     }
 
     public final static byte
@@ -27,29 +30,27 @@ public class Tokens {
         DOT             = 9,
         DOTDOT          = 10,
         BOOL_LIT        = 11,
-        OUTROS          = 12,
-        OP_REL          = 13,
-        OP_MUL          = 14,
-        OP_AD           = 15,
-        VAZIO           = 16,
-        ID              = 17,
-        TIPO_SIMPLES    = 18,  
-        IN_LIT          = 19, 
-        FLOAT_LIT       = 20, 
-        PROGRAM         = 21,
-        VAR             = 22,
-        WHILE           = 23,
-        DO              = 24,
-        IF              = 25,
-        THEN            = 26, 
-        ELSE            = 27, 
-        BEGIN           = 28, 
-        END             = 29, 
-        ARRAY           = 30,
-        OF              = 31,
-        FUNCTION        = 32,
-        PROCEDURE       = 33,
-        ERROR           = 34;
+        OP_REL          = 12,
+        OP_MUL          = 13,
+        OP_AD           = 14,
+        ERROR           = 15,
+        ID              = 16,
+        TIPO_SIMPLES    = 17,  
+        INT_LIT         = 18, 
+        FLOAT_LIT       = 19, 
+        PROGRAM         = 20,
+        VAR             = 21,
+        WHILE           = 22,
+        DO              = 23,
+        IF              = 24,
+        THEN            = 25, 
+        ELSE            = 26, 
+        BEGIN           = 27, 
+        END             = 28, 
+        ARRAY           = 29,
+        OF              = 30,
+        FUNCTION        = 31,
+        PROCEDURE       = 32;
     
     public final static String[] spellings = {
         ":=",
@@ -63,12 +64,11 @@ public class Tokens {
         ".",
         "..",
         "bool-lit",
-        "outros",
         "op-rel",
         "op-mul",
         "op-ad",
-        "vazio",
-        "<id>", 
+        "error",
+        "id", 
         "tipo-simples",
         "int-lit",
         "float-lit",
@@ -84,7 +84,6 @@ public class Tokens {
         "array", 
         "of",
         "function",
-        "procedure",
-        "error"
+        "procedure"
     };    
 }
