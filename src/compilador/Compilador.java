c8d703ec459202286a24f5180f9a258edb52a7c0
Daniele Reis <danielesreis@outlook.com>
2018-02-01 17:02:59 -0200

    classe Parser sendo feita

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -3,7 +3,10 @@ package compilador;
 
 public class Compilador {
     public static void main(String[] args) {
-
+       int i;
+       
+       for (i=0; i<args.length; i++)
+           System.out.println(args[i] + " ");
     }
     
 }
--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -2,4 +2,290 @@ package compilador;
 
 public class Parser {
     private Token currentToken;
+    
+    private void accept (byte expectedTokenKind) {
+        if (expectedTokenKind == currentToken.kind) {
+            //currentToken = next Token
+        }
+        else
+        {
+            //return error
+        }
+    }
+    
+    private void acceptIt() {
+        //currentToken = next Token
+    }
+    
+    public void parse() {
+        
+    }
+    
+    private void parsePrograma() {
+        accept(Token.PROGRAM);
+        accept(Token.ID);
+        accept(Token.SEMICOLON);
+        parseCorpo();
+        accept(Token.DOT);
+    }
+    
+    private void parseCorpo() {
+        
+        while (currentToken.kind == Token.VAR || currentToken.kind == Token.FUNCTION) {
+            parseDeclaracao();
+            accept(Token.SEMICOLON);
+        }
+        
+        accept(Token.BEGIN);
+        
+        while (currentToken.kind == Token.IF || currentToken.kind == Token.WHILE || currentToken.kind == Token.BEGIN || currentToken.kind == Token.ID) {
+            parseComando();
+            accept(Token.SEMICOLON);
+        }
+        accept(Token.END);
+    }
+    
+    private void parseDeclaracao() {
+        switch(currentToken.kind) {
+            case Token.VAR: 
+                acceptIt();
+                accept(Token.ID);
+                            
+                while(currentToken.kind == Token.COMMA) {
+                    acceptIt();
+                    accept(Token.ID);
+                }
+                            
+                accept(Token.COLON);
+                parseTipo();
+                break;
+                
+            case Token.FUNCTION: 
+                acceptIt();
+                accept(Token.ID);
+                accept(Token.LPAREN);
+                            
+                switch(currentToken.kind) {
+                    case Token.VAR: case Token.ID:
+                        parseParametros();
+                        
+                        while(currentToken.kind == Token.SEMICOLON) {
+                            acceptIt();
+                            parseParametros();
+                        }
+                        accept(Token.RPAREN);
+                        break;
+                    case Token.RPAREN: 
+                        acceptIt();
+                        break;
+                    default:
+                        //report error
+                }
+                
+                accept(Token.COLON);
+                accept(Token.TIPO_SIMPLES);
+                accept(Token.SEMICOLON);
+                parseCorpo();
+                
+            case Token.PROCEDURE: 
+                acceptIt();
+                accept(Token.ID);
+                accept(Token.LPAREN);
+                            
+                switch(currentToken.kind) {
+                    case Token.VAR: case Token.ID:
+                        parseParametros();
+                        
+                        while(currentToken.kind == Token.SEMICOLON) {
+                            acceptIt();
+                            parseParametros();
+                        }
+                        accept(Token.RPAREN);
+                        break;
+                    case Token.RPAREN: 
+                        acceptIt();
+                        break;
+                    default:
+                        //report error
+                }
+                
+                accept(Token.SEMICOLON);
+                parseCorpo();
+                break;
+                
+            default:
+                //report error
+        }
+    }
+    
+    private void parseComando() {
+          switch(currentToken.kind){
+        case Token.IF: 
+           acceptIt();
+           parseExpressao();
+           accept(Token.THEN);
+           parseComando();
+           if (currentToken.kind == Token.ELSE){
+            accept (Token.ELSE);
+            parseComando();
+           }
+           else{
+           }
+        break;
+       
+        case Token.WHILE: 
+           acceptIt();
+           parseExpressao();
+           accept(Token.DO);
+           parseComando();
+        break;
+       
+        case Token.BEGIN: 
+           acceptIt();
+           while (currentToken.kind == Token.IF || currentToken.kind == Token.WHILE ||currentToken.kind == Token.BEGIN ||currentToken.kind == Token.ID) {
+           parseComando();
+           accept(Token.SEMICOLON);
+           }
+           accept(Token.END);
+        break;
+        
+       case Token.ID: 
+           switch(currentToken.kind){
+               case Token.LBRACE:
+                   while( currentToken.kind == Token.LBRACE){
+                       acceptIt();
+                       parseExpressao();
+                       accept(Token.RBRACE);
+                   }
+                   accept(Token.BECOMES);
+                   parseExpressao();
+                break;
+
+               case Token.LPAREN:
+                   acceptIt();
+                   parseExpressao();
+                   while( currentToken.kind == Token.COMMA){
+                       acceptIt();
+                       parseExpressao();
+                   }
+                   accept(Token.RPAREN);
+                break;
+               default: //Erro Sintático
+           }
+        break;
+        
+        default: //Erro Sintático
+    }
+    }
+    
+    private void parseFator() {
+        switch(currentToken.kind){
+            case Token.ID:
+                while(currentToken.kind == Token.LBRACE){
+                   acceptIt();
+                   parseExpressao(); 
+                   accept(Token.RBRACE);
+                }
+                if(currentToken.kind == Token.LPAREN){
+                   acceptIt();
+                   parseExpressao(); 
+                   while( currentToken.kind == Token.COMMA){
+                       acceptIt();
+                       parseExpressao();
+                   }
+                   accept(Token.RPAREN);
+                }
+                break;
+            
+            case Token.BOOL_LIT:
+                acceptIt();
+                break;
+            
+            case Token.INT_LIT:
+                acceptIt();
+                break;
+            
+            case Token.FLOAT_LIT:
+                acceptIt();
+                break;
+            
+            case Token.LPAREN:
+                acceptIt();
+                parseExpressao();
+                accept(Token.RPAREN);
+                break;
+                
+            default: // Erro sintático
+        }        
+    }
+    
+    private void parseTipo() {
+        switch (currentToken.kind) {
+            case Token.ARRAY:
+                parseTipoAgregado();
+                break;
+            case Token.BOOL_LIT:
+                acceptIt();
+                break;
+            case Token.INT_LIT:
+                acceptIt();
+                break;
+            case Token.FLOAT_LIT:
+                acceptIt();
+                break;
+            default:
+                //report error
+        }
+    }
+    
+    private void parseParametros() {
+        switch (currentToken.kind) {
+            case Token.VAR:
+                acceptIt();
+                accept(Token.ID);
+                break;
+            case Token.ID:
+                acceptIt();
+                break;
+            default: 
+                //report error
+        }
+        
+        while(currentToken.kind == Token.COMMA) {
+            acceptIt();
+            accept(Token.ID);
+        }
+        
+        accept(Token.COLON);
+        accept(Token.TIPO_SIMPLES);
+    }
+    
+    private void parseTipoAgregado() {
+        switch(currentToken.kind) {
+            case Token.ARRAY: 
+                acceptIt();
+                accept(Token.LBRACE);
+                
+                if (currentToken.kind == Token.BOOL_LIT || currentToken.kind == Token.INT_LIT || currentToken.kind == Token.FLOAT_LIT)
+                    acceptIt();
+                else {
+                    //return error
+                }
+                
+                accept(Token.DOTDOT);
+                
+                if (currentToken.kind == Token.BOOL_LIT || currentToken.kind == Token.INT_LIT || currentToken.kind == Token.FLOAT_LIT)
+                    acceptIt();
+                else {
+                    //return error
+                }
+                
+                accept(Token.RBRACE);
+                accept(Token.OF);
+                parseTipo();
+                break;
+            default:
+                //return error
+        }
+    }
 }
