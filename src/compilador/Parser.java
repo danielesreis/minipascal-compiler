6f03bab2457537df252b0ad1166dfaf7dad872ae
Daniele Reis <danielesreis@outlook.com>
2018-02-08 15:56:46 -0200

    bug procedure consertado

--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -35,9 +35,87 @@ public class Parser {
         accept(Token.EOT);
     }
     
+    private void parseComando() {
+        
+        switch(currentToken.kind) {
+            
+            case Token.IF: 
+                acceptIt();
+                parseExpressao();
+                accept(Token.THEN);
+                parseComando();
+            
+                if (currentToken.kind == Token.ELSE){
+                    accept (Token.ELSE);
+                    parseComando();
+                }
+                break;
+       
+            case Token.WHILE:   
+                acceptIt();
+                parseExpressao();
+                accept(Token.DO);
+                parseComando();
+                break;
+       
+            case Token.BEGIN: 
+                acceptIt();
+                while (currentToken.kind == Token.IF || currentToken.kind == Token.WHILE ||currentToken.kind == Token.BEGIN ||currentToken.kind == Token.ID) {
+                    parseComando();
+                    accept(Token.SEMICOLON);
+                }
+                accept(Token.END);
+                break;
+        
+            case Token.ID:
+                acceptIt();
+                
+                switch(currentToken.kind) {
+                    case Token.BECOMES:
+                        acceptIt();
+                        parseExpressao();
+                        break;
+
+                    case Token.LBRACE:
+                        while( currentToken.kind == Token.LBRACE) {
+                            acceptIt();
+                            parseExpressao();
+                            accept(Token.RBRACE);
+                        }
+                        accept(Token.BECOMES);
+                        parseExpressao();
+                        break;
+
+                    case Token.LPAREN:
+                        acceptIt();
+
+                        if( currentToken.kind == Token.RPAREN )
+                            acceptIt();
+                        else if ( currentToken.kind == Token.ID ) {
+                            parseExpressao();
+
+                            while( currentToken.kind == Token.COMMA){
+                                acceptIt();
+                                parseExpressao();
+                            }
+                            accept(Token.RPAREN);
+                        }
+                        else {
+                            Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Chamada de procedimento mal formulada! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
+                        }
+                        break;
+                        
+                    default: Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
+                }
+                break;
+        
+            default: Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Comando mal formulado! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1]);
+        }
+    }
+    
     private void parseCorpo() {
         
-        while (currentToken.kind == Token.VAR || currentToken.kind == Token.FUNCTION) {
+        while (currentToken.kind == Token.VAR || currentToken.kind == Token.FUNCTION || currentToken.kind == Token.PROCEDURE) {
             parseDeclaracao();
             accept(Token.SEMICOLON);
         }
@@ -133,81 +211,32 @@ public class Parser {
         }
     }
     
-    private void parseComando() {
+    private void parseExpressao() {
         
-        switch(currentToken.kind) {
-            
-            case Token.IF: 
-                acceptIt();
-                parseExpressao();
-                accept(Token.THEN);
-                parseComando();
-            
-                if (currentToken.kind == Token.ELSE){
-                    accept (Token.ELSE);
-                    parseComando();
-                }
-                break;
-       
-            case Token.WHILE:   
-                acceptIt();
-                parseExpressao();
-                accept(Token.DO);
-                parseComando();
-                break;
-       
-            case Token.BEGIN: 
-                acceptIt();
-                while (currentToken.kind == Token.IF || currentToken.kind == Token.WHILE ||currentToken.kind == Token.BEGIN ||currentToken.kind == Token.ID) {
-                    parseComando();
-                    accept(Token.SEMICOLON);
-                }
-                accept(Token.END);
-                break;
+        parseExpressaoSimples();
         
-            case Token.ID:
-                acceptIt();
-                
-                switch(currentToken.kind) {
-                    case Token.BECOMES:
-                        acceptIt();
-                        parseExpressao();
-                        break;
-
-                    case Token.LBRACE:
-                        while( currentToken.kind == Token.LBRACE) {
-                            acceptIt();
-                            parseExpressao();
-                            accept(Token.RBRACE);
-                        }
-                        accept(Token.BECOMES);
-                        parseExpressao();
-                        break;
-
-                    case Token.LPAREN:
-                        acceptIt();
-
-                        if( currentToken.kind == Token.RPAREN )
-                            acceptIt();
-                        else if ( currentToken.kind == Token.ID ) {
-                            parseExpressao();
-
-                            while( currentToken.kind == Token.COMMA){
-                                acceptIt();
-                                parseExpressao();
-                            }
-                            accept(Token.RPAREN);
-                        }
-                        else {
-                            Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Chamada de procedimento mal formulada! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
-                        }
-                        break;
-                        
-                    default: Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
-                }
-                break;
+        if (currentToken.kind == Token.OP_REL){
+           acceptIt();
+           parseExpressaoSimples();
+        }
+   }
+   
+    private void parseExpressaoSimples(){
         
-            default: Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Comando mal formulado! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1]);
+       parseFator();
+        
+        while(currentToken.kind == Token.OP_MUL){
+            acceptIt();
+            parseFator();
+        }
+        
+        while(currentToken.kind == Token.OP_AD){
+            acceptIt();
+            parseFator();
+            while(currentToken.kind == Token.OP_MUL){
+                acceptIt();
+                parseFator();
+            }
         }
     }
     
@@ -255,22 +284,6 @@ public class Parser {
         }        
     }
     
-    private void parseTipo() {
-        switch (currentToken.kind) {
-            
-            case Token.ARRAY:
-                parseTipoAgregado();
-                break;
-            
-            case Token.TIPO_SIMPLES:
-                acceptIt();
-                break;
-            
-            default:
-                Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Tipo mal formulado! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
-        }
-    }
-    
     private void parseParametros() {
         switch (currentToken.kind) {
             
@@ -296,6 +309,22 @@ public class Parser {
         accept(Token.TIPO_SIMPLES);
     }
     
+    private void parseTipo() {
+        switch (currentToken.kind) {
+            
+            case Token.ARRAY:
+                parseTipoAgregado();
+                break;
+            
+            case Token.TIPO_SIMPLES:
+                acceptIt();
+                break;
+            
+            default:
+                Compilador.compilerFrame.setOutputText("ERRO SINTÁTICO: Tipo mal formulado! Símbolo encontrado: '" + Token.spellings[currentToken.kind-1] + "'");
+        }
+    }
+   
     private void parseTipoAgregado() {
         switch(currentToken.kind) {
             
@@ -336,32 +365,4 @@ public class Parser {
         }
     }
     
-    private void parseExpressao() {
-        
-        parseExpressaoSimples();
-        
-        if (currentToken.kind == Token.OP_REL){
-           acceptIt();
-           parseExpressaoSimples();
-        }
-   }
-   
-   private void parseExpressaoSimples(){
-        
-       parseFator();
-        
-        while(currentToken.kind == Token.OP_MUL){
-            acceptIt();
-            parseFator();
-        }
-        
-        while(currentToken.kind == Token.OP_AD){
-            acceptIt();
-            parseFator();
-            while(currentToken.kind == Token.OP_MUL){
-                acceptIt();
-                parseFator();
-            }
-        }
-    }
 }
